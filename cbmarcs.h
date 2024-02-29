4acb1eb8a9d3209778cf8f7bc34ebe66cca8480c
Dan Fandrich <dan@coneharvesters.com>
2024-02-28 21:25:00 -0800

    Fix compilation on a basic FreeBSD 14.0 installation
    This change in the path to endian.h works all the way back to at least
    FreeBSD 6. I'm guessing it worked on other systems if they were using
    gcc.

--- cbmarcs.h
+++ cbmarcs.h
@@ -35,7 +35,7 @@
 
 #include <stdio.h>
 #if (!defined(SUNOS) && !defined(sun) && !defined(_WIN32))
-#if defined(__MACH__) || defined(__DJGPP__)
+#if defined(__MACH__) || defined(__DJGPP__) || defined(__FreeBSD__)
 #include <machine/endian.h>
 #elif defined(__GNUC__)
 #include <endian.h>
