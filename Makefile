dfandrich/fvcbm

commit 7fdaf03bb363d6618ed47a7f4d15b841745c3f33

Bump FreeBSD and Alpine versions in CI

.github/workflows/ci.yml
@@ -104,10 +104,10 @@ jobs:
           name: 'fvcbm-windows.exe'
           path: fvcbm.exe
 
-  alpine-3_21-x86:
-    name: 'Alpine 3.21 with MUSL'
+  alpine-3_22-x86:
+    name: 'Alpine 3.22 with MUSL'
     runs-on: ubuntu-latest
-    container: 'alpine:3.21'
+    container: 'alpine:3.22'
     steps:
       - uses: actions/checkout@v4
       - name: 'Install deps'
@@ -188,12 +188,12 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        ver: ["13.4", "14.2"]
+        ver: ["13.5", "14.2"]
     runs-on: ubuntu-latest
     steps:
       - uses: actions/checkout@v4
       - name: 'build & test on FreeBSD'
-        uses: cross-platform-actions/action@v0.27.0
+        uses: cross-platform-actions/action@v0.28.0
         with:
           operating_system: "freebsd"
           version: ${{ matrix.ver }}
@@ -231,6 +231,7 @@ jobs:
         run: sudo apt-get install -y --no-install-suggests --no-install-recommends qemu-user-static
       - name: 'Perform configure, build, test'
         run: >
+          # TODO: reference by tag once 1.0 is released
           docker run --rm -v "$(pwd):/${{ github.workspace }}" -w "${{ github.workspace }}" --platform linux/ppc adelielinux/adelie:latest sh -c '
           cat /etc/os-release &&
           uname -a &&

commit b342bafa037a5ece38d70995c6ea0dc390f90a4d

Perform a normal build if no make target is given
Most of the specific targets are legacy, so if none is given perform a default
UNIX build.  The old help menu that was given without a target is now available
with "make help". Also, fix the Adelie build that was accidentally
(silently) broken in commit 7fdaf03b.

.github/workflows/ci.yml
@@ -44,35 +44,35 @@ jobs:
           gcc-14-powerpc64-linux-gnu
           libc6-dev-ppc64-cross
       - name: 'Compile with gcc'
-        run: make linux
+        run: make CC=gcc
       - name: 'Run tests with gcc'
         run: make test
       - name: 'Compile with gcc c99'
-        run: make clean && make linux LINUX_CFLAGS='-std=c99 -D_DEFAULT_SOURCE -O2 -Werror -Wall -Wextra -Wshadow -Wpedantic -Wcast-qual -Wcast-align -Wwrite-strings -Wno-attributes'
+        run: make clean && make CC=gcc CFLAGS='-std=c99 -D_DEFAULT_SOURCE -O2 -Werror -Wall -Wextra -Wshadow -Wpedantic -Wcast-qual -Wcast-align -Wwrite-strings -Wno-attributes'
       - name: 'Run tests with gcc c99'
         run: make test
       - name: 'Compile with clang'
-        run: make clean && make linux LINUX_CC=clang LINUX_CFLAGS='-O3 -Werror -Wall -Wextra -Wcomment -Wdeprecated -Wimplicit-fallthrough -Wmissing-prototypes -Wmissing-field-initializers -Wformat -Wformat-nonliteral -Woverlength-strings -Wshadow -Wtype-limits -Wunreachable-code -Wunused -Wvarargs -Wreturn-type -Wundef -Wuninitialized -Wunreachable-code -Wunused-function'
+        run: make clean && make CC=clang CFLAGS='-O3 -Werror -Wall -Wextra -Wcomment -Wdeprecated -Wimplicit-fallthrough -Wmissing-prototypes -Wmissing-field-initializers -Wformat -Wformat-nonliteral -Woverlength-strings -Wshadow -Wtype-limits -Wunreachable-code -Wunused -Wvarargs -Wreturn-type -Wundef -Wuninitialized -Wunreachable-code -Wunused-function'
       - name: 'Run tests with clang'
         run: make test
       - name: 'Compile with gcc 32-bit'
-        run: make clean && make linux LINUX_CC=i686-linux-gnu-gcc-14 LINUX_CFLAGS='-m32 -O2 -Werror -Wall -Wextra -Wshadow -Wpedantic -Wcast-qual -Wcast-align -Wwrite-strings -Wno-attributes' LDFLAGS=-static
+        run: make clean && make CC=i686-linux-gnu-gcc-14 CFLAGS='-m32 -O2 -Werror -Wall -Wextra -Wshadow -Wpedantic -Wcast-qual -Wcast-align -Wwrite-strings -Wno-attributes' LDFLAGS=-static
       - name: 'Run tests with gcc 32-bit'
         run: make test
       - name: 'Compile with gcc-aarch64'
-        run: make clean && make linux LINUX_CC=aarch64-linux-gnu-gcc-14 LDFLAGS=-static
+        run: make clean && make CC=aarch64-linux-gnu-gcc-14 LDFLAGS=-static
       - name: 'Run tests with gcc-aarch64'
         run: make test TESTWRAPPER=qemu-aarch64
       - name: 'Compile with gcc-arm'
-        run: make clean && make linux LINUX_CC=arm-linux-gnueabi-gcc-14 LDFLAGS=-static
+        run: make clean && make CC=arm-linux-gnueabi-gcc-14 LDFLAGS=-static
       - name: 'Run tests with gcc-arm'
         run: make test TESTWRAPPER=qemu-arm
       - name: 'Compile with gcc-powerpc'
-        run: make clean && make linux LINUX_CC=powerpc-linux-gnu-gcc-14 LDFLAGS=-static
+        run: make clean && make CC=powerpc-linux-gnu-gcc-14 LDFLAGS=-static
       - name: 'Run tests with gcc-powerpc'
         run: make test TESTWRAPPER=qemu-ppc
       - name: 'Compile with gcc-powerpc64'
-        run: make clean && make linux LINUX_CC=powerpc64-linux-gnu-gcc-14 LDFLAGS=-static
+        run: make clean && make CC=powerpc64-linux-gnu-gcc-14 LDFLAGS=-static
       - name: 'Run tests with gcc-powerpc64'
         run: make test TESTWRAPPER=qemu-ppc64
       - name: 'Set up wine'
@@ -90,11 +90,11 @@ jobs:
           # Run wine once to set it up, which displays some logs that would mess up tests
           WINEDEBUG=-all wine cmd /c exit
       - name: 'Compile with mingw32-posix'
-        run: make clean && make linux LINUX_CC=x86_64-w64-mingw32-gcc-13-posix
+        run: make clean && make CC=x86_64-w64-mingw32-gcc-13-posix
       - name: 'Run tests with mingw32-posix+wine'
         run: make test TESTWRAPPER=./wine-wrapper
       - name: 'Compile with mingw32'
-        run: make clean && make linux LINUX_CC=x86_64-w64-mingw32-gcc
+        run: make clean && make CC=x86_64-w64-mingw32-gcc
       - name: 'Run tests with mingw32+wine'
         run: make test TESTWRAPPER=./wine-wrapper
       - name: 'Strip binary'
@@ -117,7 +117,7 @@ jobs:
           build-base
           groff
       - name: 'Compile with gcc with MUSL'
-        run: make linux
+        run: make CC=gcc
       - name: 'Run tests with gcc with MUSL'
         run: make test
       - name: 'Install i386 chroot'
@@ -127,7 +127,7 @@ jobs:
           make clean
           cp -a . i386root/root/fvcbm/ || true Ignore recursive copy error
       - name: 'Compile with gcc with MUSL on i386'
-        run: chroot i386root make -C /root/fvcbm linux LINUX_CC=gcc CFLAGS='-static -m32 -O2 -Werror -Wall -Wextra'
+        run: chroot i386root make -C /root/fvcbm CC=gcc CFLAGS='-static -m32 -O2 -Werror -Wall -Wextra -Wno-attributes'
       - name: 'Run tests with gcc with MUSL on i386'
         run: chroot i386root make -C /root/fvcbm test
       - name: 'Strip binary'
@@ -151,7 +151,7 @@ jobs:
           docker run --rm -v "$(pwd):/${{ github.workspace }}" -w ${{ github.workspace }} --platform linux/arm alpine:3.21 sh -c '
           cat /etc/os-release &&
           apk add --no-cache build-base groff &&
-          make linux CFLAGS="-static -O2 -Werror -Wall -Wextra" &&
+          make CFLAGS="-static -O2 -Werror -Wall -Wextra -Wno-attributes" &&
           make test &&
           strip fvcbm
           '
@@ -170,12 +170,12 @@ jobs:
       - uses: actions/checkout@v4
       - name: 'Compile with gcc'
         # nroff is not available, so skip the man build
-        run: touch fvcbm.man && make linux
+        run: touch fvcbm.man && make CC=gcc
       - name: 'Run tests with gcc'
         run: make test
       - name: 'Compile fat binary with gcc'
         # nroff is not available, so skip the man build
-        run: make clean && touch fvcbm.man && make linux LINUX_CC=cc LINUX_CFLAGS='-O2 -arch arm64 -arch x86_64 -Werror -Wall -Wextra'
+        run: make clean && touch fvcbm.man && make CC=gcc CFLAGS='-O2 -arch arm64 -arch x86_64 -Werror -Wall -Wextra'
       - name: 'Run tests with fat binary with gcc'
         run: make test
       - uses: actions/upload-artifact@v4
@@ -201,7 +201,7 @@ jobs:
           run: |
             # heirloom-tools has the original nroff, but it isn't working with -man
             sudo pkg update -f && sudo pkg install -y groff
-            make linux LINUX_CC=cc
+            make
             make test
 
   z88dk:
@@ -230,12 +230,12 @@ jobs:
       - name: Set up dependencies
         run: sudo apt-get install -y --no-install-suggests --no-install-recommends qemu-user-static
       - name: 'Perform configure, build, test'
+        # TODO: reference by tag once 1.0 is released
         run: >
-          # TODO: reference by tag once 1.0 is released
           docker run --rm -v "$(pwd):/${{ github.workspace }}" -w "${{ github.workspace }}" --platform linux/ppc adelielinux/adelie:latest sh -c '
           cat /etc/os-release &&
           uname -a &&
           apk add --no-cache gcc musl-dev make groff &&
-          make linux &&
+          make &&
           make test
           '

Makefile
@@ -7,7 +7,11 @@ PREFIX=		/usr/local
 BINDIR=		$(PREFIX)/bin
 MANDIR=		$(PREFIX)/share/man
 
-# All the flags in $(CC) are optional and only used to generate warnings
+# Default
+CC=cc
+CFLAGS=-O2
+LDFLAGS=
+PACKFLAG=
 
 # Linux
 LINUX_CC=	gcc
@@ -44,9 +48,11 @@ CPM_CFLAGS=	+cpm -create-app
 # Items below this line need not be changed
 #
 
-all:
+all: targets
+
+help:
 	@echo ""
-	@echo "Please run make with one of the following arguments"
+	@echo "Run make with one of the following arguments for a non-default target:"
 	@echo "cpm        -- for CP/M machines with Z88DK cross-compiler"
 	@echo "dos        -- for MS-DOS with Turbo C"
 	@echo "linux      -- for Linux with gcc"
@@ -64,7 +70,7 @@ cpm:
 	$(MAKE) targets CC="$(CPM_CC)" CFLAGS="$(CPM_CFLAGS) $(CFLAGS)" PACKFLAG=""
 
 dos:
-	make -f makefile.dos
+	make -fmakefile.dos
 
 linux:
 	$(MAKE) targets CC="$(LINUX_CC)" CFLAGS="$(LINUX_CFLAGS) $(CFLAGS)" PACKFLAG=""

README
@@ -8,10 +8,10 @@ files (R/S/U/P00), emulator tape images (T64 and TAP) and emulator disk images
 (D64 and X64 and variants), including 1541, 1571, 1581, 8050 and 8250 disk
 types.
 
-To compile it under Linux, type:
-	make linux
-or type:
+To compile it under UNIX-like systems, type:
 	make
+or type:
+	make help
 to see the list of other targets.
 
 To compile under MS-DOS using Turbo C, type: